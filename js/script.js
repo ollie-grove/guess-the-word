da53453f3714a3307311ac40395b5eb317a7d36a
Ollie Grove <ollie_grove@protonmail.com>
2022-06-30 09:45:44 -0700

    Created global variables, added function to add placeholder symbol for each letter

---
+++ js/script.js
@@ -0,0 +1,41 @@
+//Unordered list of guessed letters
+const guessedLetters = document.querySelector(".guessed-letters"); 
+
+//"Guess" button
+const button = document.querySelector(".button-element"); 
+
+//Letter input box 
+const letterInput = document.querySelector(".letter");  
+
+//Empty paragrah where word in progress appears 
+const wordInProgress = document.querySelector(".word-in-progress"); 
+
+//Paragraph where remaining guesses display
+const guessesRemaining = document.querySelector(".remaining");  
+
+//Span inside remaining guesses
+const spanRemaining = document.querySelector("span");  
+
+//Paragraph where messages appear when player guesses a letter 
+const message = document.querySelector(".message"); 
+
+//Hidden button that appears prompting player to play again 
+const playAgainButton = document.querySelector(".play-again");  
+
+//First test word
+const word = ("Magnolia"); 
+
+//Circle symbols to represent each letter in the word  
+const placeholder = function (word) {
+    const placeholderLetters = []; 
+    for (const letter of word) {
+        console.log(letter); 
+        placeholderLetters.push("●");
+    }
+    wordInProgress.innerText = placeholderLetters.join(""); 
+}; 
+
+placeholder(word); 
+
+
+
